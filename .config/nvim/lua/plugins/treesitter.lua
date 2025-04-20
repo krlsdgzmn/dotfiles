7f34f6422fd3dfce4bac8a219f0dd6d8b800d20c
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2025-04-20 17:07:40 +0800

    feat: addded treesitter and lazygit

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      require("nvim-treesitter.configs").setup({
+          ensure_installed = { 
+		"c", "lua", "vim", "vimdoc", "sql", "query", "javascript", "typescript",  "tsx", "html", "css", "json", "python", "rust", "go"
+	    },
+	    sync_install = false,
+	    highlight = { enable = true },
+	    indent = { enable = true },  
+	    incremental_selection = {
+		enable = true,
+		keymaps = {
+		    init_selection = "<Enter>",
+		    node_incremental = "<Enter>",
+		    scopc_incremental = false,
+		    node_decremental = "<Backspace>",
+		},
+	    },
+
+	})
+    end
+}
