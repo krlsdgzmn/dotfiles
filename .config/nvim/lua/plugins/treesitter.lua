71faaa11b4b03946334a25e30d9f03798ba30dd9
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2025-04-21 16:16:56 +0800

    feat: added autopair and tag

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,37 +1,55 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"c",
-				"lua",
-				"vim",
-				"vimdoc",
-				"sql",
-				"query",
-				"javascript",
-				"typescript",
-				"tsx",
-				"html",
-				"css",
-				"json",
-				"python",
-				"rust",
-				"go",
-			},
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<Enter>",
-					node_incremental = "<Enter>",
-					scopc_incremental = false,
-					node_decremental = "<Backspace>",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"sql",
+					"query",
+					"javascript",
+					"typescript",
+					"tsx",
+					"html",
+					"css",
+					"json",
+					"python",
+					"rust",
+					"go",
 				},
-			},
-		})
-	end,
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<Enter>",
+						node_incremental = "<Enter>",
+						scopc_incremental = false,
+						node_decremental = "<Backspace>",
+					},
+				},
+			})
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		dependencies = {
+			{ "nvim-treesitter/nvim-treesitter" },
+		},
+		event = "InsertEnter",
+		config = true,
+		opts = {},
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		dependencies = {
+			{ "nvim-treesitter/nvim-treesitter" },
+		},
+		opts = {},
+	},
 }
