f18d157f0e08f2e4cebe912c880ed6150cf3f438
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2025-04-21 14:47:08 +0800

    feat: added more options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,5 +1,8 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+vim.g.autoformat = true
+vim.opt.formatexpr = "v:lua.require'conform'.formatexpr()"
+vim.opt.formatoptions = "jcroqlnt"
 
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -13,11 +16,15 @@ vim.opt.scrolloff = 5
 vim.opt.mouse = ""
 vim.opt.cursorline = true
 
+vim.opt.expandtab = true
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.smarttab = true
 vim.opt.smartindent = true
 vim.opt.autoindent = true
+vim.opt.breakindent = true
+vim.opt.termguicolors = true
 
 vim.opt.undofile = true
 
