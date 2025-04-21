61def374499472d4bfe617d3fd672d9c7c40b736
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2025-04-21 13:50:49 +0800

    fix: added tabstop opt

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -13,10 +13,11 @@ vim.opt.scrolloff = 5
 vim.opt.mouse = ""
 vim.opt.cursorline = true
 
--- vim.opt.shiftwidth = 4
--- vim.opt.smarttab = true
--- vim.opt.smartindent = true
--- vim.opt.autoindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 vim.opt.undofile = true
 
